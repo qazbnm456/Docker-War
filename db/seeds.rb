b6aa4aa0da3defe2d951c5824164b726c8764110
Lobsiinvok <qazbnm456@gmail.com>
2016-02-27 14:08:21 +0800

    add seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,61 @@
+#Indicator
+Indicator.create!( :value => false )
+
+#Major
+Major.create!( :name => "CHINESE LITERATURE" )
+Major.create!( :name => "FOREIGN LANGUAGES AND LITERATURE" )
+Major.create!( :name => "MUSIC" )
+Major.create!( :name => "THEATRE ARTS" )
+Major.create!( :name => "BIOLOGICAL SCIENCES" )
+Major.create!( :name => "CHEMISTRY" )
+Major.create!( :name => "PHYSICS" )
+Major.create!( :name => "APPLIED MATHEMATICS" )
+Major.create!( :name => "ELECTRICAL ENGINEERING" )
+Major.create!( :name => "MECHANICAL AND ELECTRO-MECHANICAL ENGINEERING" )
+Major.create!( :name => "COMPUTER SCIENCE AND ENGINEERING" )
+Major.create!( :name => "PHOTONICS" )
+Major.create!( :name => "MATERIALS AND OPTOELECTRONIC SCIENCE" )
+Major.create!( :name => "BUSINESS MANAGEMENT" )
+Major.create!( :name => "INFORMATION MANAGEMENT" )
+Major.create!( :name => "FINANCE" )
+Major.create!( :name => "MARINE BIOTECHNOLOGY AND RESOURCES" )
+Major.create!( :name => "MARINE ENVIRONMENT AND ENGINEERING" )
+Major.create!( :name => "OCEANOGRAPHY" )
+Major.create!( :name => "POLITICAL ECONOMY" )
+Major.create!( :name => "SOCIOLOGY" )
+Major.create!( :name => "OTHERS" )
+
+#Sex
+Sex.create!( :name => "Male" )
+Sex.create!( :name => "Female" )
+Sex.create!( :name => "Others" )
+
+#Basic
+Basic.create!( :title => "basic 1", :url => "#", :flag => "#" )
+Basic.create!( :title => "basic 2", :url => "#", :flag => "#" )
+Basic.create!( :title => "basic 3", :url => "#", :flag => "#" )
+Basic.create!( :title => "basic 4", :url => "#", :flag => "#" )
+Basic.create!( :title => "basic 5", :url => "#", :flag => "#" )
+
+#Web
+Web.create!( :title => "web 1", :url => "http://subdomain.domain.com:[:port]/", :subdomain => "#", :db => "#"  )
+Web.create!( :title => "web 2", :url => "http://subdomain.domain.com:[:port]/", :subdomain => "#", :db => "#"  )
+Web.create!( :title => "web 3", :url => "http://subdomain.domain.com:[:port]/", :subdomain => "#", :db => "#" )
+Web.create!( :title => "web 4", :url => "http://subdomain.domain.com:[:port]/", :subdomain => "#", :db => "#"  )
+Web.create!( :title => "web 5", :url => "http://subdomain.domain.com:[:port]/", :subdomain => "#", :db => "#" )
+
+#Reverse
+Reverse.create!( :title => "reverse 1", :url => "#", :flag => "#" )
+Reverse.create!( :title => "reverse 2", :url => "#", :flag => "#" )
+Reverse.create!( :title => "reverse 3", :url => "#", :flag => "#" )
+Reverse.create!( :title => "reverse 4", :url => "#", :flag => "#" )
+Reverse.create!( :title => "reverse 5", :url => "#", :flag => "#" )
+
+#Crypto
+Crypto.create!( :title => "crypto 1", :url => "#", :flag => "#" )
+Crypto.create!( :title => "crypto 2", :url => "#", :flag => "#" )
+Crypto.create!( :title => "crypto 3", :url => "#", :flag => "#" )
+Crypto.create!( :title => "crypto 4", :url => "#", :flag => "#" )
+Crypto.create!( :title => "crypto 5", :url => "#", :flag => "#" )
+
+#Forensic
